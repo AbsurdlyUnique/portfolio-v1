3230bee78ab957d8a0aed63fb2090d12da3d7bf8
Julian Richter <absurdity.discord@gmail.com>
2024-06-23 17:30:06 +0200

    edited the template to match our vision

--- uno.config.ts
+++ uno.config.ts
@@ -11,9 +11,35 @@ import {
 
 export default defineConfig({
   shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['btn', 'px-4 py-1 rounded inline-block bg-primary text-primary-foreground cursor-pointer !outline-none hover:bg-primary-600 disabled:cursor-default disabled:bg-muted disabled:opacity-50'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-primary'],
   ],
+  theme: {
+    colors: {
+      'background': 'hsl(var(--background))',
+      'foreground': 'hsl(var(--foreground))',
+      'muted': 'hsl(var(--muted))',
+      'muted-foreground': 'hsl(var(--muted-foreground))',
+      'popover': 'hsl(var(--popover))',
+      'popover-foreground': 'hsl(var(--popover-foreground))',
+      'card': 'hsl(var(--card))',
+      'card-foreground': 'hsl(var(--card-foreground))',
+      'border': 'hsl(var(--border))',
+      'input': 'hsl(var(--input))',
+      'primary': 'hsl(var(--primary))',
+      'primary-foreground': 'hsl(var(--primary-foreground))',
+      'secondary': 'hsl(var(--secondary))',
+      'secondary-foreground': 'hsl(var(--secondary-foreground))',
+      'accent': 'hsl(var(--accent))',
+      'accent-foreground': 'hsl(var(--accent-foreground))',
+      'destructive': 'hsl(var(--destructive))',
+      'destructive-foreground': 'hsl(var(--destructive-foreground))',
+      'ring': 'hsl(var(--ring))',
+    },
+    borderRadius: {
+      DEFAULT: 'var(--radius)',
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
